e311245ee90904829461a662f28d6ea88fbdce43
reniejoshi <renie_joshi@yahoo.com>
2025-12-31 20:54:08 -0800

    Refactor to use good programming conventions
    Add comments to explain code, rename function to enhance clarity, replace onclick() with addEventListener(), replace this with e.target, and replace var with const.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //https://www.w3schools.com/html/html5_webstorage.asp
 
-let nodeList = document.getElementsByTagName("LI");
+// -- Display and add functionality to elements for existing tasks --
+
+// Add close button to existing tasks
+const nodeList = document.getElementsByTagName("LI");
 for (let i = 0; i < nodeList.length; i++) {
     let span = document.createElement("SPAN");
     let txt = document.createTextNode("\u00D7");
@@ -9,32 +12,37 @@ for (let i = 0; i < nodeList.length; i++) {
     nodeList[i].appendChild(span);
 }
 
-let close = document.getElementsByClassName("close");
+// Add functionality to close buttons to delete tasks on click
+const close = document.getElementsByClassName("close");
 for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
+    close[i].addEventListener('click', function(e) {
+        const div = e.target.parentElement;
         div.style.display = "none";
-    }
+    });
 }
 
-let list = document.querySelector('ul');
+// Mark tasks as completed on click
+const list = document.querySelector('ul');
 list.addEventListener('click', function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle('checked');
     }
 }, false);
 
+// Enable draggable feature for list items
 document.querySelectorAll('LI').forEach(li => li.draggable = true);
 
-function newElement() {
+// -- Implement functionality to create new tasks
+
+// Function to create new tasks
+function createNewTask() {
     let li = document.createElement('li');
     let taskEntered = document.getElementById('taskEntered').value;
     let t = document.createTextNode(taskEntered);
     li.appendChild(t);
     if (taskEntered === '') {
         alert("Please enter your task");
-    }
-    else {
+    } else {
         document.getElementById('taskList').appendChild(li);
     }
     document.getElementById('taskEntered').value = "";
@@ -46,22 +54,23 @@ function newElement() {
     li.appendChild(span);
 
     for (let i = 0; i < close.length; i++) {
-        close[i].onclick = function () {
-            var div = this.parentElement;
+        close[i].addEventListener('click', function (e) {
+            const div = e.target.parentElement;
             div.style.display = "none";
-        }
+        });
     }    
 
     li.draggable = "true";
 }
 
+// Add event listener to create task when enter key is pressed
 taskEntered.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-        newElement();
+        createNewTask();
     }
 });
 
-//draggable feature
+// -- Implement draggable feature --
 
 const draggableList = document.getElementById('taskList');
 let draggedItem = null;
@@ -77,17 +86,16 @@ draggableList.addEventListener("dragstart", e => {
 draggableList.addEventListener("dragend", e => {
     draggedItem.classList.remove("dragging");
     setTimeout(() => {
-        draggedItem.style.display = ""; //default
+        draggedItem.style.display = "";
     }, 0);
 });
 
 draggableList.addEventListener("dragover", e => {
-    e.preventDefault(); //allows draggedItem to be dropped
+    e.preventDefault();
     const afterElement = getDragAfterElement(draggableList, e.clientY);
     if (afterElement === null) {
         draggableList.appendChild(draggedItem);
-    }
-    else {
+    } else {
         draggableList.insertBefore(draggedItem, afterElement);
     }
 });
@@ -101,8 +109,7 @@ const getDragAfterElement = (container, y) => {
 
         if (offset < 0 && offset > closest.offset) {
             return { offset: offset, element: child, };
-        }
-        else {
+        } else {
             return closest;
         }
     },
